85c5089112a7fc861dae6aa955582dd77b79311e
Oleksandr Gneushev <galexing@gmail.com>
2022-09-14 10:50:02 +0200

    first commit - all base

---
+++ hardhat/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xc311B9F8aD1ad086898a34d3868D9e174b090CB8";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL =
+  "https://whitelist-dapp-goerli-3kih5cf0i-iamgalexing.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
